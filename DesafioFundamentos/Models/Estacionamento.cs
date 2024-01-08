ab5917b2e7bb746b113ebc7cd096f4389b10af45
jaquesilva <jaqueline.natario@gmail.com>
2024-01-08 08:27:27 -0300

    Adiciona comentários no 'Program.cs' para melhor entendimento do código

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -15,11 +15,8 @@ namespace DesafioFundamentos.Models
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            
-            //IMPLEMENTADO!!!
             Console.WriteLine("Digite a placa do veículo para estacionar:");
-            string placa = Console.ReadLine();
+            string placa = Console.ReadLine(); // valor armazenado como string
 
             veiculos.Add(placa);
 
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,59 +1,66 @@
-using DesafioFundamentos.Models;
+﻿using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoInicial = 0; // variável do tipo decimal que inicia com valor "0"
+decimal precoPorHora = 0; // variável do tipo decimal que inicia com valor "0"
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" + // Cria o menu inicial
+                  "Digite o preço inicial:"); // Solicita uma entrada para o usuário
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+precoInicial = Convert.ToDecimal(Console.ReadLine()); /* tudo que entra através do Console.WriteLine é string, então devemos 
+ converter para int (inteiro)*/
+
+Console.WriteLine("Agora digite o preço por hora:"); // Solicita uma entrada para o usuário
+
+precoPorHora = Convert.ToDecimal(Console.ReadLine()); /* tudo que entra através do Console.WriteLine é string, então devemos 
+ converter para int (inteiro)*/
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
-string opcao = string.Empty;
+string opcao = string.Empty; // NÃO ENTENDI QUAL A FUNÇÃO DESSA LINHA
 bool exibirMenu = true;
 
 // Realiza o loop do menu
 while (exibirMenu)
 {
-    Console.Clear();
+    Console.Clear(); // Essa linha limpa o console, para aparecer somente esse menu, e não as linhas 9, 10 e 15 juntas
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
 
-    switch (Console.ReadLine())
+    switch (Console.ReadLine()) /* Entrada de um valor, digitado pelo usuário conforme menu acima, linhas 31 a 34, e de acordo 
+    com o número digitado, entrará em um respectivo case */
     {
-        case "1":
-            es.AdicionarVeiculo();
-            break;
+        case "1": // Caso o usuário escolha a opção 1
+            es.AdicionarVeiculo(); // Chama o método "AdicionarVeiculo" da classe "Estacionamento"
+            break; // Encerra o switch e pula para a linha 60
 
-        case "2":
-            es.RemoverVeiculo();
-            break;
+        case "2": // Caso o usuário escolha a opção 2
+            es.RemoverVeiculo(); // Chama o método "RemoverVeiculo" da classe "Estacionamento"
+            break; // Encerra o switch e pula para a linha 60
 
-        case "3":
-            es.ListarVeiculos();
-            break;
+        case "3": // Caso o usuário escolha a opção 3
+            es.ListarVeiculos(); // Chama o método "ListarVeiculos" da classe "Estacionamento"
+            break; // Encerra o switch e pula para a linha 60
 
-        case "4":
-            exibirMenu = false;
-            break;
+        case "4": // Caso o usuário escolha a opção 4
+            exibirMenu = false; // Para de exibir o menu
+            break; // Encerra o switch e pula para a linha 60
 
         default:
-            Console.WriteLine("Opção inválida");
-            break;
+            Console.WriteLine("Opção inválida"); // Caso o usuário escolha um número diferente de 1, 2, 3 ou 4, aparecerá essa msg.
+            break; // Encerra o switch e pula para a linha 60
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+    Console.WriteLine("Pressione uma tecla para continuar"); /* Qualquer tecla que o usuário digitar o programa será encerrado e 
+    aparecerá a linha 64 */
+    Console.ReadLine(); /* Por que o que foi digitado na linha 60 como entrada do usuário não precisou ser convertido aqui na 
+    linha 62? Resposta: Acredito que por não usar esse valor para um fim específico, qualquer valor que entrasse, serviria */
 }
 
-Console.WriteLine("O programa se encerrou");
+Console.WriteLine("O programa se encerrou"); // Mensagem indicando que o programa encerrou.
